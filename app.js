d0df1e551513d9413727e965848a336740dafb67
Tanner Bodrero <tanner.bodrero@gmail.com>
2019-08-02 13:52:57 -0700

    commit changes

--- app.js
+++ app.js
@@ -1,25 +1,3 @@
-// Wrap every letter in a span
-var textWrapper = document.querySelector('.searched-artist');
-textWrapper.innerHTML = textWrapper.textContent.replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>");
-
-anime.timeline({loop: false})
-  .add({
-    targets: '.searched-artist .letter',
-    scale: [4,1],
-    opacity: [0,1],
-    translateZ: 0,
-    easing: "easeOutExpo",
-    duration: 950,
-    delay: function(el, i) {
-      return 70*i;
-    }
-  }).add({
-    targets: '.searched-artist',
-    duration: 1000,
-    easing: "easeOutExpo",
-    delay: 1000
-  });
-  
   function searchSpotify() {
     var inputArtist = $("#inlineFormInput").val().trim();
     var accessToken = "BQDs6lyA21gFf6E9RS0WRnq9ZRPuO87fksre7u0yJ_kFFGlIQj2Zmqb0DtrQGyeGWVPnQ5CqEK9ac1hg-Nt5kOAe0GB6BorAEtzbmkTdE5QNoT74Bqk-GEh0PJIvJOcVG-chY_JdZ6815nI"
