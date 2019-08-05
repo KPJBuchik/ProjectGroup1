c82ed29625a6d8412adb65dbeb49113437e1dcf3
Kevin Buchik <kbuchik@gmail.com>
2019-08-05 10:20:50 -0700

    up to date

--- app.js
+++ app.js
@@ -1,123 +1,121 @@
 
-  
-  function searchSpotify() {
+function searchSpotify() {
     var inputArtist = $("#inlineFormInput").val().trim();
-    var accessToken = "BQDs6lyA21gFf6E9RS0WRnq9ZRPuO87fksre7u0yJ_kFFGlIQj2Zmqb0DtrQGyeGWVPnQ5CqEK9ac1hg-Nt5kOAe0GB6BorAEtzbmkTdE5QNoT74Bqk-GEh0PJIvJOcVG-chY_JdZ6815nI"
+    var accessToken = "BQDv6sz9Uc-_8nSQH_DoC-0pl0aW0z_IHGTNke1o0WGnc7yZtUqN4RQkIuISGdORpF6LZfAblBfiNuZT10dvGK4wV1Crr4OkDLph1I0tzKCsSq0cm6cTyVwISk-lfVqKa31UbpI-QH0d9PM"
     var queryUrl = "https://api.spotify.com/v1/search?q=" + inputArtist + "&type=artist&limit=1"
     $.ajax({
-        url: queryUrl, // 'https://api.spotify.com/v1/search',
-        method: "GET",
-        headers: {
-            'Authorization': 'Bearer ' + accessToken
-        },
+      url: queryUrl,
+      method: "GET",
+      headers: {
+        'Authorization': 'Bearer ' + accessToken
+      },
     }).then(function (response) {
-        console.log(response)
-
-        var artistName = $("#links-results").text(response.artists.items[0]
-            .external_urls.spotify)
-        // artistName.attr("href", artistName )
-        //var imgURL = $("#image-results").text(JSON.stringify((response.artists.items["0"].images["0"])))
-        //var image = $("<img>").attr("src", imgURL);
-        //imgURL.append(image);
-
-
-        $("#list").append(artistName)
-        SearchTopTracks(response.artists.items[0].id)
-
+      console.log(response)
+  
+      var artistName = $("#links-results").attr("href", response.artists.items[0]
+        .external_urls.spotify)
+      $("#links-results").text(response.artists.items[0].name)
+      var imgURL = $("#image-results").attr("src", response.artists.items[0].images[1].url)
+      console.log(imgURL)
+      SearchTopTracks(response.artists.items[0].id)
     })
-}
-
-function SearchTopTracks(id) {
+  }
+  
+  function SearchTopTracks(id) {
     var topTracks = $("#inlineFormInput").val().trim();
-    var accessToken = "BQAxtYN4aIAmj0Uz1Ziua2O7RQpb41025WH9ocsy_PiijfNA4n1ZmPZ8etk5KaAX7R78kxxfZDHr90bK8WKicSAtj9kvJO4eXH-3gTgzrA4bcbDZ8T8D7i1TitwM_bDwnoIj49bHu87uEWY"
-
+    var accessToken = "BQDv6sz9Uc-_8nSQH_DoC-0pl0aW0z_IHGTNke1o0WGnc7yZtUqN4RQkIuISGdORpF6LZfAblBfiNuZT10dvGK4wV1Crr4OkDLph1I0tzKCsSq0cm6cTyVwISk-lfVqKa31UbpI-QH0d9PM"
+  
     var queryUrl = "https://api.spotify.com/v1/artists/" + id + "/top-tracks?country=us"
-
+  
     $.ajax({
-        url: queryUrl,
-        method: "GET",
-        headers: {
-            'Authorization': 'Bearer ' + accessToken
-        },
+      url: queryUrl,
+      method: "GET",
+      headers: {
+        'Authorization': 'Bearer ' + accessToken
+      },
     }).then(function (response) {
-        console.log(response)
-        // var displayTracks = $("#track-results").text()
-
-        console.log(topTracks)
-
+      console.log(response)
+      var displayTracks = $("#track-results").attr("href", response.tracks[0].external_urls.spotify)
+      $("#track-results").text(response.tracks[0].name)
+      console.log(displayTracks)
+      $("#track-results1").attr("href", response.tracks[1].external_urls.spotify)
+      $("#track-results1").text(response.tracks[1].name)
+      $("#track-results2").attr("href", response.tracks[2].external_urls.spotify)
+      $("#track-results2").text(response.tracks[2].name)
+      trackInfo(response.tracks[0].id)
+      $("#widget").attr("src", "https://open.spotify.com/embed/album/"+response.tracks[0].album.id)
+  
     })
-}
-
-$(".btn").on("click", function (event) {
+  }
+  //
+  $(".btn").on("click", function (event) {
     event.preventDefault();
     searchSpotify();
     searchSeatGeek();
-    // SearchTopTracks();
-
-})
-
-
-function searchSeatGeek() {
-    var inputArtist = $("#inlineFormInput").val().trim();
-
-    var queryUrl = "https://api.seatgeek.com/events?performers.slug=" + inputArtist + "&client_id=MTc3MjY2OTR8MTU2NDYwNTcxOC43MQ";
-
+    $("#inlineFormInput").val("")
+    //clear value
+    //clear modal 
+  })
+  
+  
+  
+  function trackInfo(id) {
+    var trackInformation = $("#inlineFormInput").val().trim();
+    var accessToken = "BQDv6sz9Uc-_8nSQH_DoC-0pl0aW0z_IHGTNke1o0WGnc7yZtUqN4RQkIuISGdORpF6LZfAblBfiNuZT10dvGK4wV1Crr4OkDLph1I0tzKCsSq0cm6cTyVwISk-lfVqKa31UbpI-QH0d9PM"
+  
+    // var queryUrl = "https://api.spotify.com/v1/audio-features/11dFghVXANMlKmJXsNCbNl"
+    var queryUrl = "https://api.spotify.com/v1/audio-features/" + id
     $.ajax({
-        url: queryUrl, 
-        method: "GET",
-         
+      url: queryUrl,
+      method: "GET",
+      headers: {
+        'Authorization': 'Bearer ' + accessToken
+      },
     }).then(function (response) {
-
-        
-        console.log(response);
-        //location
-        //link to tickets
-        //venue
-        var eventName = $("#events-results").text(response.events[0].title)
-        console.log(artistName)
-
-        $("#list").append(eventName)
-
+      console.log(response)
+  
+      if (response.danceability > 0.6) {
+        // var danceInfo = $("<p>")
+        var danceInfo = $("#danceability").text("Can I dance to this?: Hell Yeah")
+      }
+      else if (response.danceability < 0.6)
+        $("#danceability").text("Can I dance to this?: No Way")
+      console.log(danceInfo)
+  
+  
+  
+  
+  
     })
-}
-
-/*
-//$(".inputArtist").on("click", function (event) {
-  //  event.preventDefault();
-    //searchSeatGeek();
-
-//})
-        console.log(response);
-        var artistName = $("#links-results").text(response.artists.items["0"].external_urls.spotify)
-        console.log(artistName)
-        $("#list").append(artistName)
+  }
+  function searchSeatGeek() {
+    var inputArtist = $("#inlineFormInput").val().trim();
+  
+    var queryUrl = "https://api.seatgeek.com/events?performers.slug=" + inputArtist + "&client_id=MTc3MjY2OTR8MTU2NDYwNTcxOC43MQ";
+  
+    $.ajax({
+      url: queryUrl,
+      method: "GET",
+  
+    }).then(function (response) {
+  
+  
+      console.log(response);
+      var eventName = $("#events-results").text(response.events[0].title)
+      var location = $("#location-results").text(response.events[0].venue.address)
+      var tickets = $("#ticket-results").attr("href",response.events[0].url)
+      var venue = $("venue-results").text(response.events[0].venue)    
+      console.log(artistName)
+  
+      $("#list").append(eventName)
+  
     })
-}
-$(".btn").on("click", function (event) {
-    event.preventDefault();
-    searchSpotify();
-})
-*/
-
-
-// Wrap every letter in a span
-var textWrapper = document.querySelector('.searched-artist');
-textWrapper.innerHTML = textWrapper.textContent.replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>");
-
-anime.timeline({loop: false})
-  .add({
-    targets: '.searched-artist .letter',
-    scale: [4,1],
-    opacity: [0,1],
-    translateZ: 0,
-    easing: "easeOutExpo",
-    duration: 950,
-    delay: function(el, i) {
-      return 70*i;
-    }
-  }).add({
-    targets: '.searched-artist',
-    duration: 1000,
-    easing: "easeOutExpo",
-    delay: 1000
-  });
+  }
+  
+  
+  
+  //create the html elements in javascript
+  //fix the appending on danceablity 
+  //add authorization?
+  //mark attending?
+  //play with the webplayback 
